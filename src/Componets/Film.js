64100d0272e935cfb9672b05475c2776d216fd7a
hsongra <hitesh.songra.contractor@move.com>
2019-09-14 20:08:24 +0530

    Add Favorite/Unfavorite feature

--- src/Componets/Film.js
+++ src/Componets/Film.js
@@ -4,16 +4,29 @@ import axios from "axios";
 export class Film extends Component {
   state = {
     films: [],
-    displayFilms: []
+    displayFilms: [],
+    favFilms: []
   };
 
   fetchFilms = () => {
-    return axios.get("https://swapi.co/api/films/").then(response => {
+    axios.get("https://swapi.co/api/films/").then(response => {
       this.setState({
         films: response.data.results,
         displayFilms: response.data.results
       });
+      if (this.state.favFilms.length > 0) {
+        this.sortFilmsByFav();
+      }
     });
+  };
+
+  sortFilmsByFav = () => {
+    let sortedFilms = this.state.displayFilms
+      .sort(function(film) {
+        return film.isFav ? 1 : -1;
+      })
+      .reverse();
+    this.setState({ displayFilms: sortedFilms });
   };
 
   componentDidMount() {
@@ -21,14 +34,25 @@ export class Film extends Component {
   }
 
   searchHandler = event => {
-    let searcjQery = event.target.value.toLowerCase(),
+    let searhChars = event.target.value.toLowerCase(),
       displayFilms = this.state.films.filter(el => {
         let searchValue = el.title.toLowerCase();
-        return searchValue.indexOf(searcjQery) !== -1;
+        return searchValue.indexOf(searhChars) !== -1;
       });
     this.setState({
       displayFilms: displayFilms
     });
+  };
+
+  favFilmsHandler = id => {
+    let film = this.state.displayFilms.find(function(f) {
+      return f.episode_id === id;
+    });
+    film.isFav = !film.isFav;
+    this.setState({
+      displayFilms: this.state.displayFilms
+    });
+    this.sortFilmsByFav();
   };
 
   render() {
@@ -46,9 +70,16 @@ export class Film extends Component {
         <div className='row mt-3'>
           <ul className='list-group'>
             {this.state.displayFilms.map(film => {
+              let btnClass = "ml-2 btn btn-primary"; //${film.isFav ? 'danger' : 'primary'}";
               return (
                 <li className='list-group-item p-3' key={film.episode_id}>
                   {film.title}
+                  <button
+                    type='button'
+                    className={btnClass}
+                    onClick={() => this.favFilmsHandler(film.episode_id)}>
+                    {film.isFav ? "UnFavorite" : "Favorite"}
+                  </button>
                 </li>
               );
             })}
