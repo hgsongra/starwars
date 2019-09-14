a27badb64aa88997558a748461a6e86704111e0d
hsongra <hitesh.songra.contractor@move.com>
2019-09-14 21:42:00 +0530

    Updates

---
+++ precache-manifest.8e6fccab042e6a1fcdccb1e15db74d40.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f4349d65fb5c1b3d93fc4577c2fc3bb",
+    "url": "/starwars/index.html"
+  },
+  {
+    "revision": "796b15eb07b8a990a82e",
+    "url": "/starwars/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "9727167b1fe819d81336",
+    "url": "/starwars/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "796b15eb07b8a990a82e",
+    "url": "/starwars/static/js/2.53dfc7e0.chunk.js"
+  },
+  {
+    "revision": "9727167b1fe819d81336",
+    "url": "/starwars/static/js/main.9d80aeb8.chunk.js"
+  },
+  {
+    "revision": "9e17b89cb66cb757e319",
+    "url": "/starwars/static/js/runtime~main.e8e04b7d.js"
+  }
+]);
